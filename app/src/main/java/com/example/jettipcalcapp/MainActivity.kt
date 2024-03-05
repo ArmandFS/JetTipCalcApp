2680033b0a2f4cf07a0cc6380613a07881e9921b
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-03-05 10:01:39 +0700

    Updated top header and added text input

--- app/src/main/java/com/example/jettipcalcapp/MainActivity.kt
+++ app/src/main/java/com/example/jettipcalcapp/MainActivity.kt
@@ -3,18 +3,23 @@ package com.example.jettipcalcapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jettipcalcapp.ui.theme.JetTipCalcAppTheme
 import androidx.compose.ui.unit.dp
@@ -24,7 +29,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                Text(text = "Hello again!")
+                TopHeader()
             }
 
         }
@@ -45,17 +50,30 @@ fun MyApp(content: @Composable ()-> Unit) {
 }
 @Preview
 @Composable
-fun TopHeader(){
+//pass in data for the topheader
+fun TopHeader(totalPerPerson: Double = 0.0){
     Surface(modifier = Modifier
         .fillMaxWidth()
         .height(150.dp)
-        .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))))
+        .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+        color = Color(0xFFE9D7F7)
+    )
     {
-        Column() {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            //format into 2 decimals
+            val total = "%.2f".format(totalPerPerson)
+            Text(text = "Total per Person",
+                 style = MaterialTheme.typography.headlineSmall)
+            Text(text = "$$total",
+                 style = MaterialTheme.typography.headlineSmall,
+                 fontWeight = FontWeight.ExtraBold)
         }
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
