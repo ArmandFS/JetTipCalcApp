97fb43870774845375dc445cf58cbbc50e92abca
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-03-08 11:03:35 +0700

    Update MainActivity.kt with rounded button widgets

--- app/src/main/java/com/example/jettipcalcapp/MainActivity.kt
+++ app/src/main/java/com/example/jettipcalcapp/MainActivity.kt
@@ -6,14 +6,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -25,12 +32,13 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jettipcalcapp.ui.theme.JetTipCalcAppTheme
 import androidx.compose.ui.unit.dp
 import com.example.jettipcalcapp.components.InputField
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import com.example.jettipcalcapp.ui.theme.JetTipCalcAppTheme
+import com.example.jettipcalcapp.widgets.RoundIconButton
 
 @OptIn(ExperimentalComposeUiApi::class)
 class MainActivity : ComponentActivity() {
@@ -116,7 +124,9 @@ fun BillForm(modifier: Modifier = Modifier,
         shape = RoundedCornerShape(corner = CornerSize(8.dp)),
         border = BorderStroke(width = 2.dp, color = Color.LightGray)
     ) {
-        Column {
+        Column(modifier = Modifier.padding(6.dp),
+               verticalArrangement = Arrangement.Top,
+               horizontalAlignment = Alignment.Start) {
             InputField(
                 valueState = totalBillState,
                 labelId = "Enter Bill",
@@ -128,8 +138,26 @@ fun BillForm(modifier: Modifier = Modifier,
                     onValChange(totalBillState.value.trim())
                     //Todo - onvaluechanged
                     keyboardController?.hide()
+                })
+            if (validState) {
+                Row(modifier = Modifier.padding(3.dp),
+                    horizontalArrangement = Arrangement.Start) {
+                    Text(text = "Split",
+                         modifier = Modifier.align(alignment = Alignment.CenterVertically))
+                    Spacer(modifier = Modifier.width(120.dp))
+                    Row (modifier = Modifier.padding(horizontal = 3.dp),
+                        horizontalArrangement = Arrangement.End){
+                        RoundIconButton(
+                            imageVector = Icons.Default.Remove,
+                            onClick = { /*TODO*/ })
+                        RoundIconButton(
+                            imageVector = Icons.Default.Add,
+                            onClick = { /*TODO*/ })
+                    }
                 }
-            )
+            }else{
+                Box(){}
+            }
         }
     }
 }
@@ -143,3 +171,4 @@ fun DefaultPreview() {
         }
     }
 }
+
