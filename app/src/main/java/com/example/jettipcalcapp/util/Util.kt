432c531ec986d7ca8fa7a5fdd529e13f606de897
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-03-13 16:49:07 +0700

    Update Util.kt with adding a calculate total per person function

--- app/src/main/java/com/example/jettipcalcapp/util/Util.kt
+++ app/src/main/java/com/example/jettipcalcapp/util/Util.kt
@@ -6,3 +6,10 @@ fun calculateTotalTip(totalBill: Double,
         (totalBill * tipPercentage) / 100 else 0.0
 
 }
+fun calculateTotalPerPerson(
+    totalBill: Double,
+    splitBy: Int,
+    tipPercentage: Int): Double {
+    val bill = calculateTotalTip(totalBill = totalBill, tipPercentage = tipPercentage) + totalBill
+    return (bill / splitBy)
+}
